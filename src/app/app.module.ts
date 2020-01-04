c470bec6803406f2b26488b97cdeaf51cf0dbf93
munich99 <sebastian.sawicz@bsb-muenchen.de>
2020-01-04 16:41:55 +0100

    router outlet - add to main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginComponent } from './start/login/login.component';
 import { AdminComponent } from './start/admin/admin.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, LoginComponent, AdminComponent ],
   bootstrap:    [ AppComponent ]
 })
