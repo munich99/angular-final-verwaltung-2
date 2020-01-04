bd00d4ac5a6469e4104d337dc6644bc5b537f769
munich99 <sebastian.sawicz@bsb-muenchen.de>
2020-01-04 16:01:16 +0100

    login - basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LoginComponent } from './start/login/login.component';
+import { AdminComponent } from './start/admin/admin.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, AdminComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
